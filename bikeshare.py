867934e7b231c590bd4bc3a72741eb79747ab7a5
Asad Ali <addiyali1@gmail.com>
2022-06-28 09:26:56 +0500

    Added a print statement in def get_filters

--- bikeshare.py
+++ bikeshare.py
@@ -21,7 +21,7 @@ def get_filters():
     #You will see this repeat throughout the program
 =======
     print('Hello! Let\'s explore some US bikeshare Information!')
-    print('\n\n')
+    print('\n')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 >>>>>>> refactoring
     city = ''
